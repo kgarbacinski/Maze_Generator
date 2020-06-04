da68f3f63541749023f016a2d6f940f015ddd362
Kacper <kacpergarbacinski@gmail.com>
2020-06-04 23:17:26 +0200

    Integrate with Generator and constants file

--- run.py
+++ run.py
@@ -1,18 +1,34 @@
 import pygame
 
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from Generator import Generator
+
 def init_screen(): # draw grid
     pygame.init()
     pygame.display.set_caption("Maze Generator")
 
-    display = pygame.display.set_mode()
+    display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    display.fill(pygame.Color("Black"))
+    pygame.display.update()
+
+    return display
 
 
-display = pygame.display.init()
-
-
+display = init_screen()
 
 def main():
+    generator = Generator(display)
 
+    generator.build_grid()
+    running = True
+    while(running):
+        generator.generate()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
 
 if __name__ == "__main__":
     main()
